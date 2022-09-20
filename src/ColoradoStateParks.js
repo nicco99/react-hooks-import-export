08245170d019db497119e73bf9ea5f85b3519aab
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-09-20 23:52:37 +0300

    completed la

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -4,5 +4,8 @@ import howManyParks from "./parks/howManyParks";
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return(<h1>Colorado State Parks!</h1>) 
 }
+
+
+export default ColoradoStateParks
